5d6a857953b189bcdf82d526fbb28262359f7b72
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-02-06 17:34:46 +0330

    :sparkles: Used not found method in nested dynamic route

--- app/dynamic-route/[id]/nested-dynamic-route/[nestedId]/page.tsx
+++ app/dynamic-route/[id]/nested-dynamic-route/[nestedId]/page.tsx
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 import type { DynamicRouteProps } from "../../interface";
 
 export default function NestedDynamicRoute({params} : DynamicRouteProps){
     const {id, nestedId} = params
+    if(parseInt(id) > 1000){
+        notFound()
+    }
     return <h1>{id} page of dynamic route, {nestedId} page of nested dynamic route</h1>
 }
