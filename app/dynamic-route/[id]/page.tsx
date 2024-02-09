45ec153ebfa6b25e5bed9995b49c0d773674ab53
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-02-09 19:21:28 +0330

    :mag: Created dynamic metadata title in dynamic route page

--- app/dynamic-route/[id]/page.tsx
+++ app/dynamic-route/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { Metadata } from "next";
 import type { DynamicRouteProps } from "./interface";
 
-export default function DynamicRoute({params}: DynamicRouteProps){
-    return <h1>{params.id} page of dynamic route</h1>
+export const generateMetadata = async ({ params }: DynamicRouteProps): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`page ${params.id}`);
+    }, 100);
+  });
+  return {
+    title: `Dynamic route ${title}`,
+  };
+};
+
+export default function DynamicRoute({ params }: DynamicRouteProps) {
+  return <h1>{params.id} page of dynamic route</h1>;
 }
