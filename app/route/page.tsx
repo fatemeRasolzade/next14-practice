86258a7bc54e974df22744887be5c1e962031571
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-02-10 16:58:38 +0330

    :sparkles: Added Link tag for route and home page

--- app/route/page.tsx
+++ app/route/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link"
+
 export const metadata = {
     title: "Route page"
 }
 
 export default function Route(){
-    return <h1>Route</h1>
+    return <>
+    <h1>Route</h1>
+    <Link href="/">BACK TO HOME</Link>
+    </>
 }
