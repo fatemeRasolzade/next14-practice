1e640bfade6c332cc0c6ab115cdff7754f04eebb
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-02-13 10:36:18 +0330

    :sparkles: Created active links for login and register in auth layout

--- app/(auth)/(with-auth-layout)/layout.tsx
+++ app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,13 +1,41 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+];
+
 export default function AuthLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <>
-        <h3 className="p-2 bg-green-700">Auth Header</h3>
-        {children}
-      </>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <div className="flex justify-between items-center bg-green-700 p-2">
+        <h3>Auth Header</h3>
+        <div className="flex gap-2">
+          {navLinks.map(({ name, href }) => {
+            const isActive = pathname.startsWith(href);
+            return (
+              <Link
+                href={href}
+                key={name}
+                className={
+                  isActive ? "text-white font-bold" : "text-gray-300 font-thin"
+                }
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+      {children}
+    </>
+  );
+}
